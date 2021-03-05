5343c084cf354ddce4e64f8beab6ed276511d556
vrn295 <varun.org295@gmail.com>
2021-03-06 00:41:30 +0530

    connected contact me

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -6,20 +6,32 @@ import emailIcon from '@iconify-icons/carbon/email';
 import phoneIcon from '@iconify-icons/akar-icons/phone';
 import locationAlt from '@iconify-icons/dashicons/location-alt';
 
-
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_c9fpri7', 'template_eq6vsmq', e.target, 'user_3BIAqWrBGSgeAMWtWPQtP')
+          .then((result) => {
+              console.log(result.text);
+              alert('Thank You')
+          }, (error) => {
+              console.log(error.text);
+              alert('Try Again')
+          });
+      }
     return(
         <div className='contact-container'>
             <h1 className='contact-me'>Contact <span className='color-change'>Me</span></h1>
             <div className='contact-deatils'>
                 <div className='contact-form'>
                     <h3>Message Me</h3>
-                    <form action="">
+                    <form action="" onSubmit={sendEmail}>
                         <input className='name' type="text" name='name' placeholder='Your Name' required/>
                         <input className='email' type="email" name="email" placeholder='Email' required/>
                         <input className='subject' type="text" name='subject' placeholder='Subject' required/>
-                        <textarea className='textarea' name="messange" placeholder='Message' required></textarea>
+                        <textarea className='textarea' name="message" placeholder='Message' required></textarea>
                         <button className='submit-btn' type="submit">Submit</button>
                     </form>
                 </div>
