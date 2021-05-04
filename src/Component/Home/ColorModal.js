5f71cb0f62d0181e4fe6ae9758fe5dd7f752f6dd
Varun Kumar <varun.kumar@doma.co.in>
2021-05-04 13:05:18 +0530

    Added Background color

--- src/Component/Home/ColorModal.js
+++ src/Component/Home/ColorModal.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import './ColorModal.css'
 export default function ColorModal() {
-    const colorModal = (color) =>{
+    const colorModalPrimary = (color) =>{
         var htmlElement = document.querySelector('html')
         htmlElement.setAttribute('theme-color',color)
-        // if(color === 'pink'){
-        //     htmlElement.setAttribute('theme-color',color)
-        // }
-        // else if(color === 'green'){
-        //     htmlElement.setAttribute('theme-color',color)
-        // }
-        
+    }
+    const colorModalBackground = (color) =>{
+        var htmlElement = document.querySelector('html')
+        htmlElement.setAttribute('background-color',color)
     }
     return (
         <>
             <ul className='color-box'>
-                <li onClick = {() => {colorModal("default")}} className='default'></li>
-                <li onClick = {() => {colorModal("pink")}} className='pink'></li>
-                <li onClick= {() => {colorModal("blue")}} className='blue'></li>
-                <li onClick= {() => {colorModal('purple')}} className='purple'></li>
+                <p>Primary Color</p>
+                <div>
+                    <li onClick = {() => {colorModalPrimary("default")}} className='default'></li>
+                    <li onClick = {() => {colorModalPrimary("pink")}} className='pink'></li>
+                    <li onClick= {() => {colorModalPrimary("blue")}} className='blue'></li>
+                    <li onClick= {() => {colorModalPrimary('purple')}} className='purple'></li>
+                </div>
+                <p>Background Color</p>
+                <div>
+                <li onClick = {() => {colorModalBackground("black")}} className='black'></li>
+                <li onClick = {() => {colorModalBackground("white")}} className='white'></li>
+                <li onClick= {() => {colorModalBackground("light")}} className='light'></li>
+                <li onClick= {() => {colorModalBackground('dark')}} className='dark'></li>
+                </div>
             </ul>
         </>
     )
